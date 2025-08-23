de5fea8b8ac595f437c5cc7c1de0594e7073aed1
shivdatt15 <shivdattlakhe999@gmail.com>
2025-08-23 21:02:20 +0530

    link backend and front

--- Frontend/src/api/user.api.js
+++ Frontend/src/api/user.api.js
@@ -1,26 +1,26 @@
 import axiosInstance from "../utils/axiosInstance"
 
 export const loginUser = async (password,email) =>{
-    const {data} = await axiosInstance.post("/api/auth/login",{email,password})
+    const {data} = await axiosInstance.post("/auth/login",{email,password})
     return data
 }
 
 export const registerUser = async (name,password,email) =>{
-    const {data} = await axiosInstance.post("/api/auth/register",{name,email,password})
+    const {data} = await axiosInstance.post("/auth/register",{name,email,password})
     return data
 }
 
 export const logoutUser = async () =>{
-    const {data} = await axiosInstance.get("/api/auth/logout")
+    const {data} = await axiosInstance.post("/auth/logout")
     return data
 }
 
 export const getCurrentUser = async () =>{
-    const {data} = await axiosInstance.get("/api/auth/me")
+    const {data} = await axiosInstance.get("/auth/me")
     return data
 }
 
 export const getAllUserUrls = async () =>{
-    const {data} = await axiosInstance.post("/api/user/urls")
+    const {data} = await axiosInstance.post("/user/urls")
     return data
 }
--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // dynamic from .env
-    timeout: 10000, // 10s
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api", 
+  withCredentials: true, //  send/receive cookies
+  timeout: 10000,
 });
 
 
